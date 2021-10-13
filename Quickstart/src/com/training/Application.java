9d89bf626e5a71264b106273028877edd8939b51
Iranna <iranna.kokatanur2@gmail.com>
2021-10-13 18:45:02 +0530

    quick

---
+++ Quickstart/src/com/training/Application.java
@@ -0,0 +1,12 @@
+package com.training;
+
+//import org.training.Student;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	}
+
+}
