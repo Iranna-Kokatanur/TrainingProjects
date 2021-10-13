88f0a48c7355fe32d04aff91b6faafd2258cf48a
Iranna <iranna.kokatanur2@gmail.com>
2021-10-13 18:46:22 +0530

    rest-api

---
+++ rest-api-consumer/src/main/java/com/example/demo/RestApiConsumerApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class RestApiConsumerApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx= SpringApplication.run(RestApiConsumerApplication.class, args);
+		WebClient client=ctx.getBean(WebClient.class);
+		Flux<String>resp=client.get().uri("http://localhost:8080/api/v1/students").retrieve().bodyToFlux(String.class);
+		System.out.println(resp.blockFirst());
+		
+	}
+		
+		@Bean
+		WebClient.Builder builder(){
+			return WebClient.builder();
+		}
+		@Bean
+	    WebClient client(WebClient.Builder builderRef) {
+	        return builderRef.build();
+	    }
+		
+	}
+
+
