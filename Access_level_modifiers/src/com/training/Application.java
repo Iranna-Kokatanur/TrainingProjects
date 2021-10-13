4801a769457f6508d9b8ea5963c5f815dfdb2d14
Iranna <iranna.kokatanur2@gmail.com>
2021-10-13 18:23:08 +0530

    access level modifiers

---
+++ Access_level_modifiers/src/com/training/Application.java
@@ -0,0 +1,15 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student ram= new Student();
+		
+//		System.out.println(ram.roll_number); it is private
+		System.out.println(ram.mark_scored); 
+		System.out.println(ram.email);
+		System.out.println(ram.student_name); 
+	}
+
+}
---
+++ Access_level_modifiers/src/org/training/SecondApplication.java
@@ -0,0 +1,18 @@
+package org.training;
+
+import com.training.Student;
+
+public class SecondApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Student ram= new Student();
+		
+//		System.out.println(ram.roll_number); it is private
+//		System.out.println(ram.mark_scored); only for same package
+		System.out.println(ram.email);
+//		System.out.println(ram.student_name); protected file
+
+
+}
+}
