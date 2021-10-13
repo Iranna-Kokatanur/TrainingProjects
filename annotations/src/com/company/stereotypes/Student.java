1a72809fab583d4f1122d67560f961aa52ad4c57
Iranna <iranna.kokatanur2@gmail.com>
2021-10-13 18:24:14 +0530

    annotations

---
+++ annotations/src/com/company/stereotypes/Student.java
@@ -0,0 +1,27 @@
+package com.company.stereotypes;
+
+ 
+
+import com.company.stereotypes.*;
+
+ 
+
+@Table(tableName = "Student")
+public class Student {
+    
+	@Column(name="rollNUmber")
+    private int id;
+    
+	@Column(name="firstName")
+    private String name;
+    
+    
+    
+    public Student() {
+        super();
+    }
+
+ 
+
+}
+ 
