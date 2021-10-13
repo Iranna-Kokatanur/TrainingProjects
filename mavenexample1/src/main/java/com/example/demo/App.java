84eae0fba66addc0d45d555be5fe1cafca32ea2b
Iranna <iranna.kokatanur2@gmail.com>
2021-10-13 18:43:10 +0530

    maven

---
+++ mavenexample1/src/main/java/com/example/demo/App.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Logger log = Logger.getRootLogger();
+        log.info( "Hello World!" );
+    }
+}
