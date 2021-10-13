2ebf664e41ceb7b7406c20bef61478070ffce8f3
Iranna <iranna.kokatanur2@gmail.com>
2021-10-13 18:42:21 +0530

    listeners

---
+++ listeners_filters/src/main/java/com/company/LogFilter1.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class LogFilter1
+ */
+@WebFilter("/*")
+public class LogFilter1 implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LogFilter1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	/**
+	 *
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		HttpServletRequest req = (HttpServletRequest)request;
+		String reqUri=req.getRequestURI();
+		System.out.println("pre method called"+reqUri);
+		chain.doFilter(request, response);
+		System.out.println("post method called");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ listeners_filters/src/main/java/com/company/ReqListener.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.ServletRequestAttributeListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+/**
+ * Application Lifecycle Listener implementation class ReqListener
+ *
+ */
+@WebListener
+public class ReqListener implements HttpSessionAttributeListener, ServletRequestAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ReqListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeRemoved(ServletRequestAttributeEvent)
+     */
+    public void attributeRemoved(ServletRequestAttributeEvent srae)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeAdded(ServletRequestAttributeEvent)
+     */
+    public void attributeAdded(ServletRequestAttributeEvent srae)  { 
+         // TODO Auto-generated method stub
+    	Object valueAdded= srae.getServletRequest().getAttribute("info");
+    	System.out.println("attribute= "+valueAdded+" =>Set in request");
+    }
+
+	/**
+     * @see ServletRequestAttributeListener#attributeReplaced(ServletRequestAttributeEvent)
+     */
+    public void attributeReplaced(ServletRequestAttributeEvent srae)  { 
+         // TODO Auto-generated method stub
+    	
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent event)  { 
+         // TODO Auto-generated method stub
+    	
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent event)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent event)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
